b609cd3ddc8621ecf825164b667a93e39c76a56f
churmuzache <hurmuzache.ciprian@gmail.com>
2018-07-02 07:21:02 +0100

    - added registered countries - added new key in register, one for each country code

--- src/main/newsapp_server.go
+++ src/main/newsapp_server.go
@@ -10,8 +10,8 @@ import (
 )
 
 const apiKey = "a5f5253063f74a8f9902e1e9cb8e9e53"
-const redisNewsKey = "news.key"
-const topHeadlinesUS = "https://newsapi.org/v2/top-headlines?country=us&apiKey="
+const redisNewsKey = "news.key."
+const topHeadlines = "https://newsapi.org/v2/top-headlines?country="
 
 type Source struct {
 	Id   string
@@ -39,12 +39,36 @@ var client = redis.NewClient(&redis.Options{
 	DB:       0,  // use default DB
 })
 
+var registeredCountryCodes = map[string]string{
+	"gb": "United Kingdom",
+	"ro": "Romania",
+	"us": "United States"}
+
+func getCountryFromRequest(r *http.Request) string {
+	keys, ok := r.URL.Query()["country"]
+	country := ""
+	if !ok || len(keys) < 1 {
+		country = "us"
+	} else {
+		country = string(keys[0])
+	}
+	return country
+}
+
 func getHeadlines(w http.ResponseWriter, r *http.Request) {
-	savedNews := client.Get(redisNewsKey)
+	country := getCountryFromRequest(r)
+	if registeredCountryCodes[country] == "" {
+		fmt.Println("Country not registered, default it to US news")
+		country = "us"
+	}
+	redisKey := redisNewsKey + country
+	savedNews := client.Get(redisKey)
 
 	if savedNews.Err() == redis.Nil {
 		fmt.Println("News not in redis, call api")
-		const url = topHeadlinesUS + apiKey
+
+		url := topHeadlines + country + "&apiKey=" + apiKey
+		fmt.Println(url)
 		resp, err := http.Get(url)
 
 		if err != nil {
@@ -55,17 +79,21 @@ func getHeadlines(w http.ResponseWriter, r *http.Request) {
 		var news = &News{}
 		json.Unmarshal(body, news)
 
-		err = client.Set(redisNewsKey, body, 15 * time.Minute).Err()
+		err = client.Set(redisKey, body, 15*time.Minute).Err()
 		if err != nil {
 			panic(err)
 		}
-		w.WriteHeader(200)
 		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,access-control-allow-origin, access-control-allow-headers")
+		w.WriteHeader(200)
 		w.Write(body)
 	} else {
 		fmt.Println("News in redis, don't call api")
-		w.WriteHeader(200)
 		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,access-control-allow-origin, access-control-allow-headers")
+		w.WriteHeader(200)
 		b, _ := savedNews.Bytes()
 		w.Write(b)
 	}
